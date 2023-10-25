45697a11e91f8d355391fb69dc2c5333e7033f4b
Jessel1994 <jesseldhokia@gmail.com>
2023-10-25 10:58:24 +0100

    added comments section for the individual article with some styles

--- src/Components/ViewSingleArticle.jsx
+++ src/Components/ViewSingleArticle.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
-import { getAllArticles, getArticleByArticleId } from "../axios";
+import { getCommentsByArticleId, getArticleByArticleId } from "../axios";
 import { useParams } from "react-router-dom";
 
-function ViewSingleArticle(props) {
+function ViewSingleArticle() {
     const routeParams = useParams()
+    const [comments, setComments] = useState([{
+        "comment_id": 124,
+        "body": "Vitae laudantium molestiae neque ut dicta fuga similique. Sit nesciunt magni sit beatae. Porro recusandae aut exercitationem eligendi voluptas. Dolore eligendi inventore magni voluptatem quia ut ut.",
+        "article_id": 34,
+        "author": "grumpy19",
+        "votes": -1,
+        "created_at": "2020-10-17T23:05:00.000Z"
+        }])
     
     const [viewArticle, SetViewArticle] = useState([
         {
@@ -18,6 +26,7 @@ function ViewSingleArticle(props) {
         }
     ])
     const [isLoading, setIsLoading] = useState(false);
+    const [commentLoading, setCommentLoading] = useState(false)
     
     
     const articleViewer = async () => {
@@ -33,29 +42,83 @@ function ViewSingleArticle(props) {
     
 
     return isLoading ? (<p>Loading...</p>) :
-    <article>
+   ( <main className="article-view-main">
+         <article>
         <ul>
-            <div key={viewArticle.article_id}>
+            <article key={viewArticle.article_id}>
                 <li className="article">
                     <p>#{viewArticle.article_id}</p>
                                 
                     <h4>{viewArticle.title}</h4>
                     <p>Topic: {viewArticle.topic}</p>
                     <img className="article-image" src={viewArticle.article_img_url} alt="article picture"/>
-                    <p>{viewArticle.body}</p>
+                    <p className="article-body">{viewArticle.body}</p>
                     <p>Uploaded: {viewArticle.created_at}</p>
                     <p>Author: {viewArticle.author}</p>
 
-                    <p>Comments: {viewArticle.comment_count}</p>
+                    
                                 
 
                 </li>
-            </div>
                 
+            </article>
             
+
+                
         </ul>
+        
+       
     </article>
+    <CommentSection isLoading={isLoading} setComments={setComments} setCommentLoading={setCommentLoading} comments={comments} routeParams={routeParams} />
+   </main> 
+  
+    )
    
+}
+
+function CommentSection({commentLoading, setCommentLoading, setComments, comments, routeParams}) {
+    
+    const commentViewer = async () => {
+        setCommentLoading(true)
+        const {data: {comments}} = await getCommentsByArticleId(routeParams.article_id)
+        setCommentLoading(false)
+        setComments(comments)
+        
+    }
+    useEffect(() => {
+        commentViewer()
+    }, [])
+
+    return commentLoading ? <h2>Loading...</h2> : (
+        <section className="comments-section">
+            <h2>COMMENTS</h2>
+            <ul className="comment-list">
+                {comments.map((comment) => {
+                    const timestamp = comment.created_at
+                    const date = new Date(timestamp)
+                    const options = {year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }
+                    const formattedDate = date.toLocaleDateString('en-GB', options)
+                    return (
+                        <div key={comment.comment_id}>
+                            <li className="comment-item" >
+                            <p>Posted at: {formattedDate}</p>
+                                <p>#{comment.comment_id}</p>
+                                <p>Posted By: {comment.author}</p>
+                                <p className="comment-body">{comment.body}</p>
+                                
+                                <p>VOTES: {comment.votes}</p>
+                                
+                            </li>
+                        </div>
+                        
+                    )
+                    
+                })}
+            </ul>
+        </section>
+            
+        
+    )
 }
 
 export default ViewSingleArticle;
